02589a9be92b81af3f54e3f523d8fbf30404ed83
Julien Cartigny <julien.iguchi-cartigny@univ-lille1.fr>
2019-12-10 14:05:22 +0100

    first commit

---
+++ tp14/hello.py
@@ -0,0 +1,32 @@
+import click
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/healthz')
+def health():
+    return {'message': 'Healthy'} 
+
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static', path)
+
+@app.route('/')
+def hello_world():
+    return """
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Hello world</title>
+  </head>
+  <body>
+     Hello World !!!!<br/>
+     <img src="static/flask.png"> 
+  </body>
+</html>
+"""
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
